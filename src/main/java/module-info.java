81bcda1153f36be557e53b978ce95f2bea4c8a5d
Kelemen Péter <peter.kelemen456@gmail.com>
2023-06-23 23:20:14 +0300

    Added JLink and JPackage plugins

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,10 @@
-module com.example.windowsactivator {
+module activator.windowsactivator {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.tinylog.api;
 
 
     opens activator.windowsactivator to javafx.fxml;
+
     exports activator.windowsactivator;
 }
